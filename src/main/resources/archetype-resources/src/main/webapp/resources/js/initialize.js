2b9bbd82c17de94578af5037229d758458ddfc1f
Bernardo <programming@wandrell.com>
2017-08-07 00:07:24 +0200

    Corrections to comments

--- src/main/resources/archetype-resources/src/main/webapp/resources/js/initialize.js
+++ src/main/resources/archetype-resources/src/main/webapp/resources/js/initialize.js
@@ -1,12 +1,9 @@
 /**
+ * ${projectName} javascript initialization.
+ * Copyright ${currentYear} ${developerName}
  * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-/**
+ * 
  * Loads all the Javascript functions used by the site.
- *
- * This requires access to the code in the following Javascript files:
- * - chevron-toggle.js
- * - smooth-scroll.js
  */
 
 /**

38aab9e948869a192d1d617da9d233281788c9d6
Bernardo <programming@wandrell.com>
2019-11-10 20:55:21 +0100

    Removed AJAX page

--- src/main/resources/archetype-resources/src/main/webapp/resources/js/initialize.js
+++ src/main/resources/archetype-resources/src/main/webapp/resources/js/initialize.js
@@ -9,38 +9,4 @@
 /**
  * Initializes scripts when the document loads.
  */
-$(document).ready(function() {
-
-   // Initializes data tables
-	$(".dataTable").DataTable({
-		"renderer" : "bootstrap",
-		"order" : [],
-		"columnDefs" : [ {
-			"targets" : "no-sort",
-			"orderable" : false,
-		}, {
-			"targets" : "hidden",
-			"visible" : false,
-		} ]
-	});
-
-   $(".dataTableAjax").DataTable({
-      "renderer" : "bootstrap",
-      "order" : [],
-      "columnDefs" : [ {
-         "targets" : "no-sort",
-         "orderable" : false,
-      }, {
-         "targets" : "hidden",
-         "visible" : false,
-      } ],
-      "columns" : [ {
-         "content" : "name"
-      } ],
-      "ajax" : {
-         "url" : "/rest/entity",
-         "dataSrc" : "content"
-      },
-   });
-
-});
+$(document).ready(function() {});

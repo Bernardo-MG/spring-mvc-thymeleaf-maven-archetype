0ff607bb81062f4d222c235435eec9fd1b18916a
Bernardo-MG <programming@bernardo-mg.com>
2022-12-15 23:15:52 +0100

    Corrected 404 redirection

--- src/main/resources/archetype-resources/src/main/java/config/WebConfiguration.java
+++ src/main/resources/archetype-resources/src/main/java/config/WebConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerF
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -17,16 +16,10 @@ public class WebConfiguration implements WebMvcConfigurer {
         super();
     }
 
-    @Override
-    public void addViewControllers(final ViewControllerRegistry registry) {
-        registry.addViewController("/notFound")
-            .setViewName("forward:/");
-    }
-
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
         return container -> {
-            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/notFound"));
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
         };
     }
 

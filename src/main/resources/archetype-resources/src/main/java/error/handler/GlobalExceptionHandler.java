6400b6e634641c57ea89ee42f01723d05a8dffb3
Bernardo-MG <programming@bernardo-mg.com>
2022-12-21 09:33:29 +0100

    Removed unused imports

--- src/main/resources/archetype-resources/src/main/java/error/handler/GlobalExceptionHandler.java
+++ src/main/resources/archetype-resources/src/main/java/error/handler/GlobalExceptionHandler.java
@@ -27,8 +27,6 @@ package ${package}.error.handler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
